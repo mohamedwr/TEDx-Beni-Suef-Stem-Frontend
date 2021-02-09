428fbbf69b71bdf3e77419c2821764c46757e41f
devien george <devo@pop-os.localdomain>
2021-02-09 19:25:40 +0200

    fixing page events

--- pages/events.js
+++ pages/events.js
@@ -1,9 +1,11 @@
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
 const events = () => {
 	return (
 		<>
 			<Nav />
-
-			<Footer/>
+			<h1 className='text-center text-red-500 font-bold font-roboto'>SOON</h1>
+			<Footer />
 		</>
 	);
 };
