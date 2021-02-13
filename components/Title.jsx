790b14b95b986f5012e1dd992d87bdfb2138344d
deviengeorge <deviengeorge99@gmail.com>
2021-02-14 01:36:06 +0200

    adding some SEO meta tags

--- components/Title.jsx
+++ components/Title.jsx
@@ -2,6 +2,30 @@ import Head from 'next/head';
 
 const Title = ({ children = '' }) => (
 	<Head>
+		<meta property='og:title' content='European Travel Destinations' />
+		<meta property='og:description' content='TEDxYouth@BeniSuefSTEM Webpage' />
+		<meta
+			property='og:image'
+			content='https://tedxbenisuef.vercel.app/logoWhite.png'
+		/>
+		<meta property='og:url' content='https://tedxbenisuef.vercel.app' />
+		<meta name='twitter:title' content='TEDxYouth@BeniSuefSTEM Webpage' />
+		<meta name='twitter:description' content='TEDxYouth@BeniSuefSTEM Webpage' />
+		<meta
+			name='twitter:image'
+			content=' https://tedxbenisuef.vercel.app/logoWhite.png'
+		/>
+		<meta name='twitter:card' content='summary_large_image' />
+		<meta
+			name='description'
+			content='TEDxYouth@BeniSuefSTEM aims to spot the
+								light on the idea and the concept of overcoming the tough times
+								and convert them into a huge success. In addition, spread and
+								discuss the ideas of our local STEM community, which includes
+								hundreds of students with bright minds. We plan to spread
+								practical steps for success, to rise with our people, community,
+								and our lovely country, Egypt.'
+		/>
 		<title>TEDx BeniSuefStem | {children}</title>
 	</Head>
 );
