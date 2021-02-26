a341895cca1a7a0c0327cf11947867cb30ee2591
deviengeorge <deviengeorge99@gmail.com>
2021-02-26 17:02:51 +0300

    second issue

--- pages/api/telegram_message.js
+++ pages/api/telegram_message.js
@@ -3,12 +3,19 @@ import axios from 'axios';
 export default async function handler(req, res) {
 	if (req.method === 'POST') {
 		const { email, name, message, subject } = req.body;
-		const chatId = process.env.TELEGRAM_DEVIEN_CHAT_ID;
+		const chatIdYoussef = process.env.TELEGRAM_YOUSSEF_CHAT_ID;
+		const chatIdDevien = process.env.TELEGRAM_DEVIEN_CHAT_ID;
 		const botToken = process.env.TELEGRAM_BOT_API;
 
 		try {
 			await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-				chat_id: chatId,
+				chat_id: chatIdYoussef,
+				text: `<i><b>CONTACT US</b></i>\n\n<b>Name:</b> \t${name}\n<b>Email:</b> \t${email}\n<b>Subject:</b> \t${subject}\n<b>Message:</b> \t${message}`,
+				parse_mode: 'HTML',
+			});
+
+			await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+				chat_id: chatIdDevien,
 				text: `<i><b>CONTACT US</b></i>\n\n<b>Name:</b> \t${name}\n<b>Email:</b> \t${email}\n<b>Subject:</b> \t${subject}\n<b>Message:</b> \t${message}`,
 				parse_mode: 'HTML',
 			});
