9033f4ee9c8cefb2216fe8695350d4203ac50988
deviengeorge <deviengeorge99@gmail.com>
2021-04-27 21:37:41 +0200

    updating the SEO Settings

--- pages/_document.js
+++ pages/_document.js
@@ -11,28 +11,36 @@ class MyDocument extends Document {
 			<Html>
 				<Head>
 					<meta charset='utf-8' />
+					<meta
+						name='viewport'
+						content='width=device-width, initial-scale=1.0'
+					/>
 					<meta name='language' content='english' />
-
-
 					<meta http-equiv='content-type' content='text/html' />
 					<meta name='robots' content='index,follow' />
 					<meta name='revisit-after' content='7 days' />
 					<meta name='distribution' content='web' />
 					<meta name='robots' content='noodp' />
 					<meta name='rating' content='general' />
+
+					{/* CopyRight Meta */}
 					<meta name='copyright' content='Copyright 2022' />
+
+					{/* Location Meta */}
 					<meta name='city' content='Beni Suef' />
 					<meta name='country' content='Egypt' />
-					<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 
 					<meta name='rating' content='safe for kids' />
 
+					{/* Open Graph Meta Tags */}
 					<meta property='og:type' content='website' />
 					<meta property='og:url' content='http://tedxyouthbstem.com' />
-
 					<meta property='og:site_name' content='TEDx Youth@BeniSuef STEM' />
 					<meta property='og:url' content='http://tedxyouthbstem.com' />
+
 					<meta name='twitter:site' content='http://tedxyouthbstem.com' />
+
+					{/* Author, Design, Publisher */}
 					<meta name='author' content='Devien George' />
 					<meta name='designer' content='Devien George' />
 					<meta name='publisher' content='Devien George' />
@@ -43,7 +51,7 @@ class MyDocument extends Document {
 						rel='stylesheet'
 					/>
 					<link
-						href='https://font		s.googleapis.com/css2?family=Cairo:wght@400;700;900&display=swap'
+						href='https://fonts.googleapis.com/css2?family=Cairo:wght@400;700;900&display=swap'
 						rel='stylesheet'
 					/>
 				</Head>
