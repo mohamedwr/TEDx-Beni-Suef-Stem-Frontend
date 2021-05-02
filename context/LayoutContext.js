fcc3909fe8815c29469714104f66f8ec6c16c39c
deviengeorge <deviengeorge99@gmail.com>
2021-05-02 13:18:55 +0200

    set the dark theme as default theme

--- context/LayoutContext.js
+++ context/LayoutContext.js
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from 'react';
 const LayoutContext = createContext();
 
 const LayoutProvider = ({ children }) => {
-	const [dark, setDark] = useState(false);
+	const [dark, setDark] = useState(true);
 	const [firstLoad, setFirstLoad] = useState(false);
 
 	const toggleDark = () => {
